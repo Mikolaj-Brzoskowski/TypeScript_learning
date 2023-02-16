3229a4486fe91fdaf0fd7bc4cf33e27b462339a8
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-02-16 18:43:19 +0100

    added links to slides

--- Drimiral/frontend/src/components/MainPage/MainPageSlides.tsx
+++ Drimiral/frontend/src/components/MainPage/MainPageSlides.tsx
@@ -1,4 +1,6 @@
 import { Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
 
 export interface MainPageSlidesProps {
 
@@ -6,41 +8,47 @@ export interface MainPageSlidesProps {
 
 const MainPageSlides: React.FC<MainPageSlidesProps> = ({ }) => {
 return (
-    <Carousel fade>
-        <Carousel.Item interval={10000}>
-            <img
-            className="d-block w-100"
-            src="assets/music_sleep.jpg"
-            alt="First slide"
-            />
-            <Carousel.Caption bsPrefix='custom-caption'>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+    <Carousel fade id="introduction">
+       <Carousel.Item interval={10000}>
+            <HashLink to="/home#about">	
+                <img
+                className="d-block w-100"
+                src="assets/music_sleep.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption bsPrefix='custom-caption'>
+                <h3>Welcome to Drimiral!</h3>
+                <p>App that is all about your dreams. Read the introduction to know more about.</p>
+                </Carousel.Caption>
+            </HashLink>
         </Carousel.Item>
         <Carousel.Item interval={10000}>
+            <Link to="/journal">
             <img
             className="d-block w-100"
             src="assets/MIMIR.jpg"
             alt="Second slide"
             />
             <Carousel.Caption bsPrefix='custom-caption'>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Start your journal now!</h3>
+            <p>Click to create a free account.</p>
             </Carousel.Caption>
+            </Link>
         </Carousel.Item>
         <Carousel.Item interval={10000}>
+            <Link to="/contact">
             <img
             className="d-block w-100"
             src="assets/sleeping_woman.jpg"
             alt="Third slide"
             />
             <Carousel.Caption bsPrefix='custom-caption'>
-            <h3>Third slide label</h3>
+            <h3>Do you have any suggestions or ideas what to improve?</h3>
             <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                Contact us right now! We are taking in consideration users feedback.
             </p>
             </Carousel.Caption>
+            </Link>
         </Carousel.Item>
         </Carousel>
         )
